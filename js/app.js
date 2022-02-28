fdc8f2816952572cf812c91857318c753e92995e
Prokash Pul <pprokash102@gmail.com>
2022-02-28 08:17:48 +0600

    display show post

--- js/app.js
+++ js/app.js
@@ -13,9 +13,36 @@ const sportsDataLoad = (player) => {
         .then(data => playerDataDisplay(data))
         .catch(err => console.log(err.statusText))
 }
-sportsDataLoad('')
+
+
+// load window all post
+window.addEventListener('load', () => {
+    sportsDataLoad('')
+})
+
 
 // show data in display
 const playerDataDisplay = (data) => {
-    console.log(data)
+    console.log(data.player)
+    const players = document.getElementById('player-result');
+    data.player.forEach(player => {
+        console.log(player)
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+                    <img src="${player.strCutout?player.strCutout:"img/blankimg.jpg"}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
+        `
+        players.appendChild(div);
+    })
+
 }
